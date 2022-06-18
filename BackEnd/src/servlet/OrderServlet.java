ba23e7a5f88df982998b52f4605f5e631c1c21b4
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2022-06-18 13:09:12 +0530

    load customer using customer cmb

--- BackEnd/src/servlet/OrderServlet.java
+++ BackEnd/src/servlet/OrderServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author : Rajith Sanjaya
@@ -15,11 +23,64 @@ import java.io.IOException;
  * @created : 2022 June 17
  **/
 
-@WebServlet
+@WebServlet(urlPatterns = "/order")
 public class OrderServlet extends HttpServlet {
 
     @Resource(name = "java:comp/env/jdbc/pool")
     DataSource dataSource;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String object = req.getParameter("object");
+        PrintWriter writer = resp.getWriter();
+        try {
+        switch (object){
+            case "customer" :
+                    String cusID = req.getParameter("cusID");
+                    resp.setContentType("application/json");
+                    Connection connection = dataSource.getConnection();
+                    PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer WHERE id = ?");
+                    preparedStatement.setObject(1,cusID);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+                    while (resultSet.next()){
+                        String id = resultSet.getString(1);
+                        String name = resultSet.getString(2);
+                        String address = resultSet.getString(3);
+                        String salary = resultSet.getString(4);
+
+                        System.out.println(id+":"+name+":"+address+":"+salary);
+
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("id",id);
+                        objectBuilder.add("name",name);
+                        objectBuilder.add("address",address);
+                        objectBuilder.add("salary",salary);
+                        arrayBuilder.add(objectBuilder.build());
+                    }
+                    JsonObjectBuilder response = Json.createObjectBuilder();
+                    response.add("status",201);
+                    response.add("message","Done");
+                    response.add("data",arrayBuilder.build());
+                    writer.print(response.build());
+
+                    connection.close();
+
+                break;
+            case "item" :
+
+                break;
+        }
+
+    } catch (SQLException e) {
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        objectBuilder.add("status", 500);
+        objectBuilder.add("message", "Update Failed");
+        objectBuilder.add("data", e.getLocalizedMessage());
+        writer.print(objectBuilder.build());
+    }
+        
+    }
 
 
 }
--- FrontEnd/src/controllers/OrderController.js
+++ FrontEnd/src/controllers/OrderController.js
@@ -24,8 +24,10 @@ function loadAllCusID() {
         url:"http://localhost:8080/BackEnd_Web_exploded/customer",
         method:"GET",
         success:function (resp) {
+            let defaultOp = `<option >Select Customer ID</option>`;
+            $("#idCmb").append(defaultOp);
             for (const customer of resp.data){
-                let row = `<option value= {i}>${customer.id}</option>`;
+                let row = `<option >${customer.id}</option>`;
                 $("#idCmb").append(row);
                 i = i+1;
             }
@@ -46,11 +48,14 @@ function loadAllItemCode() {
         url:"http://localhost:8080/BackEnd_Web_exploded/item",
         method:"GET",
         success:function (resp) {
+            let defaultOp = `<option >Select Item Code</option>`;
+            $("#itemIdCmb").append(defaultOp);
             for (const item of resp.data){
-                let row = `<option value= {i}>${item.id}</option>`
+                let row = `<option>${item.id}</option>`
                 $("#itemIdCmb").append(row);
                 i = i+1;
             }
+
 
 
         },error:function (ob,errorStatus,t) {
@@ -61,3 +66,56 @@ function loadAllItemCode() {
     })
 }
 
+
+$(function loadCustomer(){
+    $('#idCmb').trigger('change');
+    $('#idCmb').change(function(){
+        var cusID= $(this).val();
+        console.log(cusID);
+        $.ajax({
+            url:"http://localhost:8080/BackEnd_Web_exploded/order?object=customer&cusID="+cusID,
+            method:"GET",
+            success:function (resp) {
+                for (const customer of resp.data){
+                    $("#inCusName").val(customer.name);
+                    $('#inCusAddress').val(customer.address);
+                    $("#inCusSalary").val(customer.salary);
+                }
+
+            },
+            error:function (ob, errorStatus,t) {
+                console.log(ob)
+                console.log(errorStatus)
+                console.log(t)
+            }
+        })
+
+    });
+});
+
+
+$(function loadItem(){
+    $('#itemIdCmb').trigger('change');
+    $('#itemIdCmb').change(function(){
+        var itemCode= $(this).val();
+        console.log(itemCode);
+
+        $.ajax({
+            url:"http://localhost:8080/BackEnd_Web_exploded/order?itemCode"+itemCode,
+            method:"GET",
+            success:function (resp) {
+                for (const item of resp.data){
+
+                }
+
+
+
+            },error:function (ob,errorStatus,t) {
+                console.log(ob)
+                console.log(errorStatus)
+                console.log(t)
+            }
+        })
+
+    });
+});
